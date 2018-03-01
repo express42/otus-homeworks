23075bfc6e7bb319b75775c97f152ed63cd4082f
Andrey Aleksandrov <postgred@gmail.com>
2018-03-01 20:17:02 +0300

    check test runner file

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 echo HOMEWORK:$TRAVIS_PULL_REQUEST_BRANCH
-./homeworks/$TRAVIS_PULL_REQUEST_BRANCH/run.sh
+HOMEWORK_RUN="./homeworks/$TRAVIS_PULL_REQUEST_BRANCH/run.sh"
+
+if [ -f $HOMEWORK_PATH ]; then
+  echo "Install Docker"
+  echo "Clone repository with tests"
+  echo "..."
+  ./homeworks/$TRAVIS_PULL_REQUEST_BRANCH/run.sh
+else
+  echo "We don't have tests for this homework"
+  exit 0
+fi
