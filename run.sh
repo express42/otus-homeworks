a05c64e2be8b68bc408b9eb125d8d7d93b1322da
Andrey Aleksandrov <postgred@gmail.com>
2018-03-01 20:37:42 +0300

    Git clone 2

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@ echo HOMEWORK:$TRAVIS_PULL_REQUEST_BRANCH
 HOMEWORK_RUN="./homeworks/$TRAVIS_PULL_REQUEST_BRANCH/run.sh"
 
 echo "Clone repository with tests"
-git clone https://github.com/express42/otus-homeworks.git .
+git clone https://github.com/express42/otus-homeworks.git
 
 ls -l
 
-if [ -f homeworks/$TRAVIS_PULL_REQUEST_BRANCH/run.sh ]; then
+if [ -f otus-homeworks/homeworks/$TRAVIS_PULL_REQUEST_BRANCH/run.sh ]; then
   echo "Install Docker"
   echo "..."
   ./homeworks/$TRAVIS_PULL_REQUEST_BRANCH/run.sh
