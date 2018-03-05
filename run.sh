9611da40b76b3b14a0c1457ac987c82559d66721
Andrey Aleksandrov <postgred@gmail.com>
2018-03-05 19:02:55 +0300

    Remove unstable instructions

--- run.sh
+++ run.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 echo HOMEWORK:$TRAVIS_PULL_REQUEST_BRANCH
-HOMEWORK_RUN="./homeworks/$TRAVIS_PULL_REQUEST_BRANCH/run.sh"
 
 echo "Clone repository with tests"
 git clone https://github.com/express42/otus-homeworks.git
 
-ls -l
-
-if [ -f otus-homeworks/homeworks/$TRAVIS_PULL_REQUEST_BRANCH/run.sh ]; then
+if [ -f ./otus-homeworks/homeworks/$TRAVIS_PULL_REQUEST_BRANCH/run.sh ]; then
   echo "Install Docker"
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-  sudo apt-get update
-  sudo apt-get -y install docker-ce
-
-  echo "Run InSpec"
-  docker run --rm -v $(pwd):/home ubuntu bash "cd /home/ && ls -l"
-  # docker run --rm -v $(pwd):/share chef/inspect sh "ls -l"
-  # docker run --rm -v $(pwd):/share chef/inspec sh otus-homeworks/homeworks/$TRAVIS_PULL_REQUEST_BRANCH/run.sh
+  echo "..."
+  echo "Run tests"
 else
   echo "We don't have tests for this homework"
   exit 0
