830c8688f99c0965575a4e11b42ba849dd7a85e8
Andrey Aleksandrov <postgred@gmail.com>
2018-03-06 00:27:36 +0300

    Install Docker

--- run.sh
+++ run.sh
@@ -19,7 +19,11 @@ git clone -b $GROUP --single-branch $REPO
 
 if [ -f $HOMEWORK_RUN ]; then
   echo "Install Docker"
-  echo "..."
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt-get update
+  sudo apt-get -y install docker-ce
+
   echo "Run tests"
   ./$HOMEWORK_RUN
 else
