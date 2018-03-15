aa6fd0ea18bfb3223a537dfb1321904d7a6d7111
chromko <mich.dima@gmail.com>
2018-03-15 18:27:28 +0300

    init hw-05 tests; Check test reddit app installation

---
+++ homeworks/cloud-testapp/controls/deploy.rb
@@ -0,0 +1,71 @@
+# encoding: utf-8
+# copyright: 2018, The Authors
+
+title 'cloud-testapp: deploy'
+
+control 'Check README.md' do
+
+  describe parse_config_file('README.md') do
+    its('testapp_IP') { should match /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/ }
+  end
+  describe parse_config_file('README.md') do
+    its('testapp_PORT') { should match /^(?:[0-9]{1,5}\.)$/ }
+  end
+
+end
+
+testapphost = parse_config_file('README.md').testapp_IP
+testappport = parse_config_file('README.md').testapp_PORT
+
+control 'Configuration' do
+  title 'Check testapp installation scenarios'
+
+  %w(ruby-full ruby-bundler build-essential mongodb-org).each do |pkg|
+      describe package(pkg) do
+        it { should be_installed }
+    end
+  end
+
+  describe service('mongod') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe port(27017) do
+    it { should be_listening }
+    its('protocols') { should include 'tcp' }
+    its('processes') { should include 'mongod' }
+  end
+
+  describe port(testapphost) do
+    it { should be_listening }
+    its('protocols') { should include 'tcp' }
+    its('processes') { should include 'puma' }
+  end
+
+end
+
+control 'Reddit-APP' do
+
+  describe host(testapphost, port: testappport, protocol: 'tcp') do
+    it { should be_reachable }
+  end
+
+  describe command("curl http://#{testapphost}:#{testappport}}/signup -F 'username=travis' -F  'password=travis'")
+    its('exit_status') { should eq 0 }
+  end
+
+  describe command("curl http://#{testapphost}:#{testappport}/new -F 'title=travis-test' -F  'link=https://travis-ci.org/'")
+    its('exit_status') { should eq 0 }
+  end
+
+  describe http('http://#{testapphost}:#{testappport}}/') do
+      its('status') { should eq 200 }
+      its('body') { should cmp 'https://travis-ci.org' }
+  end
+end
+
+
+
+
+
---
+++ homeworks/cloud-testapp/controls/structure.rb
@@ -0,0 +1,35 @@
+# encoding: utf-8
+# copyright: 2018, The Authors
+
+title 'play-travis: structure'
+
+control 'structure' do
+  title 'Check files structure'
+
+  describe file('README.md') do
+    it { should exist }
+  end
+
+  describe file('install_ruby.sh') do
+    it { should exist }
+    it { should be_allowed('execute') }
+    its('content') {should match(%r{\n\Z})}    
+  end
+
+  describe file('install_mongodb.sh') do
+    it { should exist }
+    it { should be_allowed('execute') }
+    its('content') {should match(%r{\n\Z})}
+  end
+
+  describe file('deploy.sh') do
+    it { should exist }
+    it { should be_allowed('execute') }
+    its('content') {should match(%r{\n\Z})}    
+  end
+
+  describe directory('VPN') do
+    it { should exist }
+  end
+
+end
