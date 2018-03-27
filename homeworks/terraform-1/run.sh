b5d748156c3823123a5aa876c347eb35623b96cd
Nikolay Antsiferov <nklya@hotmail.com>
2018-03-27 11:03:10 +0300

    Update run.sh for terraform-1

--- homeworks/terraform-1/run.sh
+++ homeworks/terraform-1/run.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+set -e
+
+# Create keys
+touch $(awk -F '=' '/^public_key_path/ { print $2 }' terraform/terraform.tfvars.example)
+touch $(awk -F '=' '/^private_key_path/ { print $2 }' terraform/terraform.tfvars.example)
+
+# Run InSpec
 inspec exec otus-homeworks/homeworks/terraform-1
