d944b3ecce24b089744ded329f5213cdf283b1be
Nikolay Antsiferov <nklya@hotmail.com>
2018-03-27 12:07:49 +0300

    Fix keys creation

--- homeworks/terraform-1/run.sh
+++ homeworks/terraform-1/run.sh
@@ -2,8 +2,7 @@
 set -e
 
 # Create keys
-touch $(awk -F '=' '/^public_key_path/ { print $2 }' terraform/terraform.tfvars.example)
-touch $(awk -F '=' '/^private_key_path/ { print $2 }' terraform/terraform.tfvars.example)
+touch ~/.ssh/appuser.pub ~/.ssh/appuser
 
 # Run InSpec
 inspec exec otus-homeworks/homeworks/terraform-1
