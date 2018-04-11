8dc8dc735fce56eccc0f8cedf63cfba822ff7ba0
Nikolay Antsiferov <nklya@hotmail.com>
2018-04-11 19:11:40 +0300

    Add tests for ansible-4

---
+++ homeworks/ansible-4/controls/ansible.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+# copyright: 2018, The Authors
+
+title 'ansible validation'
+
+control 'ansible' do
+  impact 1
+  title 'Run ansible validation'
+
+  describe command('cd ansible && ansible-galaxy install -r environments/stage/requirements.yml') do
+    its('exit_status') { should eq 0 }
+  end
+
+  describe command('cd ansible && ansible-lint playbooks/site.yml --exclude=roles/jdauphant.nginx') do
+    its('stdout') { should eq '' }
+    its('exit_status') { should eq 0 }
+  end
+
+  describe command('find ansible/playbooks -name "*.yml" -type f -print0 | ANSIBLE_ROLES_PATH=ansible/roles xargs -0 -n1 ansible-playbook --syntax-check') do
+    its('stderr') { should_not match (/The error appears to have been/) }
+    its('exit_status') { should eq 0 }
+  end
+end
---
+++ homeworks/ansible-4/controls/structure.rb
@@ -0,0 +1,59 @@
+# encoding: utf-8
+# copyright: 2018, The Authors
+
+title 'ansible-3: structure'
+
+control 'structure' do
+  title 'Check repo structure'
+
+  dirs = [
+    'ansible',
+    'ansible/playbooks',
+    'ansible/roles',
+    'ansible/old',
+    'ansible/roles/app',
+    'ansible/roles/db',
+    'ansible/roles/db/molecule',
+    'ansible/environments',
+    'ansible/environments',
+    'ansible/environments/stage',
+    'ansible/environments/stage/group_vars',
+    'ansible/environments/prod',
+    'ansible/environments/prod/group_vars',
+  ]
+
+  files = [
+    '.gitignore',
+    'README.md',
+    'ansible/requirements.txt',
+    'ansible/ansible.cfg',
+    'ansible/Vagrantfile',
+    'ansible/playbooks/packer_app.yml',
+    'ansible/playbooks/packer_db.yml',
+    'ansible/playbooks/site.yml',
+    'ansible/playbooks/users.yml',
+    'ansible/playbooks/base.yml',
+    'ansible/playbooks/app.yml',
+    'ansible/playbooks/db.yml',
+    'ansible/playbooks/deploy.yml',
+    'ansible/environments/stage/requirements.yml',
+    'ansible/environments/prod/requirements.yml',
+  ]
+
+  dirs.each do |path|
+    describe directory(path) do
+      it { should exist }
+    end
+  end
+
+  files.each do |path|
+    describe file(path) do
+      it { should exist }
+      its('content') { should match(%r{\n\Z}) }
+    end
+  end
+
+  describe file('ansible/requirements.txt') do
+    its('content') { should match /molecule/ }
+  end
+end
