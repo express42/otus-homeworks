2d6b55f14bd9ec8fd26ff2e8658571c12745ce6a
Nikolay Antsiferov <nklya@hotmail.com>
2018-04-11 14:50:54 +0300

    Add version print

--- homeworks/ansible-1/run.sh
+++ homeworks/ansible-1/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
 
+# Check versions
+ansible --version
+ansible-lint --version
+
 # Run InSpec
 inspec exec otus-homeworks/homeworks/ansible-1
--- homeworks/ansible-2/run.sh
+++ homeworks/ansible-2/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
 
+# Check versions
+ansible --version
+ansible-lint --version
+packer version
+
 # Run InSpec
 inspec exec otus-homeworks/homeworks/ansible-2
