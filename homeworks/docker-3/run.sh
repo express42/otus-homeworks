224a2e28b76bc2c5024bc1f307fc8bd0141b14cc
Nikolay Antsiferov <nklya@hotmail.com>
2018-05-17 14:16:54 +0300

    Fix paths for docker-3

--- homeworks/docker-3/run.sh
+++ homeworks/docker-3/run.sh
@@ -3,11 +3,11 @@ set -e
 
 # Run InSpec
 # Had to split because with > 10 min builds TravisCI fails
-inspec exec otus-homeworks/homeworks/docker-3/structure.rb
+inspec exec otus-homeworks/homeworks/docker-3/controls/structure.rb
 inspec exec otus-homeworks/homeworks/docker-3/controls/build_comment.rb
 inspec exec otus-homeworks/homeworks/docker-3/controls/build_post.rb
 inspec exec otus-homeworks/homeworks/docker-3/controls/build_ui.rb
-inspec exec otus-homeworks/homeworks/docker-3/docker.rb
+inspec exec otus-homeworks/homeworks/docker-3/controls/docker.rb
 
 # Show images & status
 docker images
