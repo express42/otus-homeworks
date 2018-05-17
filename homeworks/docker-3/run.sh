2bc17630a65bd24cec06a596f7d2b3bb77ab9571
Nikolay Antsiferov <nklya@hotmail.com>
2018-05-17 14:12:36 +0300

    Define all profiles for docker-3

--- homeworks/docker-3/run.sh
+++ homeworks/docker-3/run.sh
@@ -2,10 +2,12 @@
 set -e
 
 # Run InSpec
+# Had to split because with > 10 min builds TravisCI fails
+inspec exec otus-homeworks/homeworks/docker-3/structure.rb
 inspec exec otus-homeworks/homeworks/docker-3/controls/build_comment.rb
 inspec exec otus-homeworks/homeworks/docker-3/controls/build_post.rb
 inspec exec otus-homeworks/homeworks/docker-3/controls/build_ui.rb
-inspec exec otus-homeworks/homeworks/docker-3
+inspec exec otus-homeworks/homeworks/docker-3/docker.rb
 
 # Show images & status
 docker images
