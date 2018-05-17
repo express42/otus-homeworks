65b304156be5513360b7e9afe66386a82ad0132f
Nikolay Antsiferov <nklya@hotmail.com>
2018-05-17 15:17:11 +0300

    Let's try another approach

--- homeworks/docker-3/controls/docker.rb
+++ homeworks/docker-3/controls/docker.rb
@@ -13,35 +13,41 @@ endpoints = [
   'http://comment:9292/healthcheck',
   'http://post:5000/healthcheck',
   'http://ui:9292/healthcheck',
-  ]
+]
+
+containers = [
+  "docker run -d --network=#{network} --network-alias=post_db --network-alias=comment_db mongo:latest",
+  "docker run -d --network=#{network} --network-alias=post #{user}/post:#{version}",
+  "docker run -d --network=#{network} --network-alias=comment #{user}/comment:#{version}",
+  "docker run -d --network=#{network} -p 9292:9292 --name=ui #{user}/ui:#{version}",
+]
 
 control 'docker' do
   title 'Check docker build'
 
-  # describe command("cd src/ui && USER_NAME=#{user} bash docker_build.sh") do
-  #   its('stderr') { should eq '' }
-  #   its('exit_status') { should eq 0 }
-  # end
-
-  # describe command("cd src/comment && USER_NAME=#{user} bash docker_build.sh") do
-  #   its('stderr') { should eq '' }
-  #   its('exit_status') { should eq 0 }
-  # end
-
-  # describe command("cd src/post-py && USER_NAME=#{user} bash docker_build.sh") do
-  #   its('stderr') { should eq '' }
-  #   its('exit_status') { should eq 0 }
-  # end
-
-  describe command("docker run -d --network=#{network} --network-alias=post_db --network-alias=comment_db mongo:latest && "\
-                    "docker run -d --network=#{network} --network-alias=post #{user}/post:#{version} && "\
-                    "docker run -d --network=#{network} --network-alias=comment #{user}/comment:#{version} && "\
-                    "docker run -d --network=#{network} -p 9292:9292 --name=ui #{user}/ui:#{version}") do
-    # its('stderr') { should eq '' }
+  describe command("cd src/ui && USER_NAME=#{user} bash docker_build.sh") do
+    its('stderr') { should eq '' }
     its('exit_status') { should eq 0 }
   end
 
-  describe command('sleep 15') do
+  describe command("cd src/comment && USER_NAME=#{user} bash docker_build.sh") do
+    its('stderr') { should eq '' }
+    its('exit_status') { should eq 0 }
+  end
+
+  describe command("cd src/post-py && USER_NAME=#{user} bash docker_build.sh") do
+    its('stderr') { should eq '' }
+    its('exit_status') { should eq 0 }
+  end
+
+  containers.each do |run_cmd|
+    describe command(run_cmd) do
+      # its('stderr') { should eq '' }
+      its('exit_status') { should eq 0 }
+    end
+  end
+
+  describe command('sleep 20') do
     its('exit_status') { should eq 0 }
   end
 
--- homeworks/docker-3/run.sh
+++ homeworks/docker-3/run.sh
@@ -2,12 +2,7 @@
 set -e
 
 # Run InSpec
-# Had to split because with > 10 min builds TravisCI fails
-inspec exec otus-homeworks/homeworks/docker-3/controls/structure.rb
-inspec exec otus-homeworks/homeworks/docker-3/controls/build_comment.rb
-inspec exec otus-homeworks/homeworks/docker-3/controls/build_post.rb
-inspec exec otus-homeworks/homeworks/docker-3/controls/build_ui.rb
-inspec exec otus-homeworks/homeworks/docker-3/controls/docker.rb
+inspec exec otus-homeworks/homeworks/docker-3
 
 # Show images & status
 docker images
