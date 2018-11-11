e44ac5c2ea37692d28c08bd7f8cd9481bab6a0a7
Alexander Suleymanov <asu@express42.com>
2018-11-11 16:43:13 +0300

    Expanded excludes for find
    Ansible syntax checks is skipped for inventory*.yml to allow use of dynamic inventory plugins, such as GCP

--- homeworks/ansible-2/controls/ansible.rb
+++ homeworks/ansible-2/controls/ansible.rb
@@ -7,7 +7,7 @@ control 'ansible' do
   impact 1
   title 'Run ansible validation'
 
-  describe command('find ansible ! -name inventory.yml -name "*.yml" -type f -print0 | xargs -0 -n1 ansible-playbook --syntax-check') do
+  describe command('find ansible ! -name "inventory*.yml" -name "*.yml" -type f -print0 | xargs -0 -n1 ansible-playbook --syntax-check') do
     its('stderr') { should_not match (/The error appears to have been/) }
     its('exit_status') { should eq 0 }
   end
