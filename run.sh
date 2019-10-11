99879450720c438aa7e49fb16a287fcfd6b73235
Stanislav Sturov <sstyrov@express42.com>
2019-10-11 17:30:57 +0300

    Freeze image version (0.5)

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ GROUP=2019-05
 BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
 HOMEWORK_RUN=./otus-homeworks/homeworks/$BRANCH/run.sh
 REPO=https://github.com/express42/otus-homeworks.git
-DOCKER_IMAGE=express42/otus-homeworks
+DOCKER_IMAGE=express42/otus-homeworks:0.5
 
 echo GROUP:$GROUP
 
