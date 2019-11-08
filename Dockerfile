d62a27e6e437984b8cc61fd3595733c727645b03
ivango812 <ivan@newbusinesslogic.ru>
2019-11-08 13:44:28 +0300

    Added some docker image fix

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM ubuntu:16.04
 
-LABEL version="0.7" maintainer="Express42"
+LABEL version="0.7" maintainer="ivango812"
 
 ARG PACKER_VER=1.4.4
 ARG TERRAFORM_VER=0.12.8
--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 GROUP=2019-08
 BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
 HOMEWORK_RUN=./otus-homeworks/homeworks/$BRANCH/run.sh
-REPO=https://github.com/express42/otus-homeworks.git
-DOCKER_IMAGE=express42/otus-homeworks:0.6
+REPO=https://github.com/ivango812/otus-homeworks.git
+DOCKER_IMAGE=ivango812/otus-homeworks:0.7
 
 echo GROUP:$GROUP
 
