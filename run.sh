902d8c07aa6dd0c6b29a13a0a5aa5f4c6f3c55ac
mrgreyves <mrgreyves@gmail.com>
2020-09-04 16:36:45 +0300

    add 2020-08 group

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-GROUP=2020-05
+GROUP=2020-08
 BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
 HOMEWORK_RUN=./otus-homeworks/homeworks/$BRANCH/run.sh
 REPO=https://github.com/express42/otus-homeworks.git
